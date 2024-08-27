c55e70b725094f4be3d27661ec6cdac9b1601485
zhen1007 <zhen1007@outlook.com>
2024-08-27 17:04:38 -0400

    task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -22,12 +22,50 @@ const isValid = (username) => {
 const authenticatedUser = (username, password) => {
   //returns boolean
   //write code to check if username and password match the one we have in records.
+  let validusers = users.filter((user) => {
+    return user.username === username && user.password === password;
+  });
+  // Return true if any valid user is found, otherwise false
+  if (validusers.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 //only registered users can login
 regd_users.post("/login", (req, res) => {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check if username or password is missing
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+
+  // Authenticate user
+  if (authenticatedUser(username, password)) {
+    // Generate JWT access token
+    let accessToken = jwt.sign(
+      {
+        data: password,
+      },
+      "access",
+      { expiresIn: 60 }
+    );
+
+    // Store access token and username in session
+    req.session.authorization = {
+      accessToken,
+      username,
+    };
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res
+      .status(208)
+      .json({ message: "Invalid Login. Check username and password" });
+  }
 });
 
 // Add a book review
