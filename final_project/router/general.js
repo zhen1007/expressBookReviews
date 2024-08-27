5661173759286fef3f82fbe8886d0696838e0295
zhen1007 <zhen1007@outlook.com>
2024-08-27 16:56:58 -0400

    task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -58,7 +58,13 @@ public_users.get("/title/:title", function (req, res) {
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const book = books[req.params.isbn];
+
+  if (book) {
+    return res.send(JSON.stringify(book.reviews));
+  }
+
+  return res.status(500).json({ message: "Not found the book" });
 });
 
 module.exports.general = public_users;
