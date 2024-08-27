ed9c6dd00ae65fe35193f481c7933d4f353a3533
zhen1007 <zhen1007@outlook.com>
2024-08-27 16:55:34 -0400

    task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -44,7 +44,15 @@ public_users.get("/author/:author", function (req, res) {
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+  const booksByTitle = Object.values(books).filter(
+    (book) => book.title === title
+  );
+  if (booksByTitle.length) {
+    return res.send(JSON.stringify({ booksByTitle }));
+  }
+
+  return res.status(500).json({ message: "Not found the books by title" });
 });
 
 //  Get book review
