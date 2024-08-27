2ec07b7b06aa626af7c8a1b471d8bcba1a872d23
zhen1007 <zhen1007@outlook.com>
2024-08-27 17:18:13 -0400

    task 13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -77,17 +77,21 @@ public_users.get("/author/:author", async function (req, res) {
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+public_users.get("/title/:title", async function (req, res) {
   //Write your code here
-  const title = req.params.title;
-  const booksByTitle = Object.values(books).filter(
-    (book) => book.title === title
-  );
-  if (booksByTitle.length) {
-    return res.send(JSON.stringify({ booksByTitle }));
+  try {
+    const title = req.params.title;
+    const response = await axios.get(`BOOK_SHOP_URL/title/${title}`);
+    const book = response.data;
+    if (response.result) {
+      return res.send(JSON.stringify(book));
+    }
+    return res.status(500).json({ message: "Not found the books by title" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error fetching books", error: error.message });
   }
-
-  return res.status(500).json({ message: "Not found the books by title" });
 });
 
 //  Get book review
